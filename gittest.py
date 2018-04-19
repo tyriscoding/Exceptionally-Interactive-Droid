f99e930bac1bd0c7bb668f1a2f721af54c31fa21
tyriscoding <34968132+tyriscoding@users.noreply.github.com>
2018-04-19 09:31:54 +0300

    Add files via upload

---
+++ gittest.py
@@ -0,0 +1,12 @@
+#This code is built for test
+# HELLO SIMAY AND ONUR
+#SUDO APT-GET SAY
+import os
+
+print("I will request to download some libraries for you")
+print("Then I will respond to you..")
+os.system('sudo apt-get install gnustep-gui-runtime')
+time.sleep(2)
+os.system('say  hello sime I')
+
+os.system('say  hello On oore')
